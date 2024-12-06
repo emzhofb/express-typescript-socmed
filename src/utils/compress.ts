b8608ed4427477963bff0b4c76eb05f3062e665b
Ikhda Muhammad Wildani <ikhdamuhammad@gmail.com>
2024-12-06 22:23:15 +0700

    set compressed to folder uploads

--- src/utils/compress.ts
+++ src/utils/compress.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import sharp from 'sharp';
 
 export const compressImage = async (filePath: string) => {
-  const outputFile = 'compressed_' + path.basename(filePath);
+  const outputFile = 'uploads/compressed_' + path.basename(filePath);
   await sharp(filePath)
     .resize(800) // Resize image width to 800px (you can adjust it)
     .toFile(outputFile);
